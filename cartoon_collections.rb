121506b8e44c4bcf03b181c116fb440ee259a830
Sierra Wallace <sskwallace@gmail.com>
2020-08-12 01:20:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |element, index|
+    "#{index.to_i + 1}.#{element}"
+  end
+  roll_call_dwarves(array)
 end
 
 def summon_captain_planet# code an argument here
