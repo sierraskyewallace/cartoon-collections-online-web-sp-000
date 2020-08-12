9d14995aa0e421a0d688eb6c5666106312afb3e9
Sierra Wallace <sskwallace@gmail.com>
2020-08-12 01:35:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)
   calls.any? {|word| word.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+array.find do |item|
+  cheese_types.include?(item)
 end
